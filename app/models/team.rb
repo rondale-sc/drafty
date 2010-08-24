8db78bd1cddb9badb8b791d21dcfde31c51d6f39
Jonathan Jackson <jonj@promedicalinc.com>
2010-08-24 14:32:28 -0400

    More stuff.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,28 @@
 class Team < ActiveRecord::Base
   has_many :players
-  
-  scope :with_pick, lambda {|pick_number| 
-    draft_order = pick_number % 14
-    draft_order = 14 if draft_order == 0
-    where("draft_order = ?", draft_order) 
-  } 
+
+  def self.with_pick(pick_number)
+    team_count = Team.count
+    @draft_order ||= by_draft_order.all
+    @reversed_draft_order ||= by_reversed_draft_order.all
+    
+    round = (pick_number / team_count) + (pick_number % team_count == 0 ? 0 : 1)
+
+    draft_order = pick_number % team_count
+    draft_order = team_count if draft_order == 0
+    draft_order -= 1
+    
+    rel = if round % 2 == 0
+      # EVEN  
+      @reversed_draft_order
+    else
+      # ODD
+      @draft_order
+    end
+    
+    rel[draft_order]
+  end
   
   scope :by_draft_order, order('draft_order')
-  
+  scope :by_reversed_draft_order, order('draft_order desc')
 end
-
