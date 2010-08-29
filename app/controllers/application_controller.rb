ee98502b2c6d43f0c1752debc7c8a472bfb13960
Robert Jackson <robertj@promedicalinc.com>
2010-08-29 00:09:54 -0400

    Add before_filter to allow entering admin mode from any controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_admin
+
+  def check_admin
+    if params[:admin] == 'true'
+      authenticate_or_request_with_http_basic do |username, password|
+        if username == "admin" && password == "ransom"
+          session[:admin] = true
+        end
+      end
+    end
+  end
 end
