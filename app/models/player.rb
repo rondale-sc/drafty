cef65c5c92ab8ebf906146e67b357ea4cb28dfb3
Robert Jackson <robertj@promedicalinc.com>
2011-08-19 17:59:07 -0400

    Add Player#populate and Player#update_rankings to Player, and update associated rake tasks.

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,13 +5,57 @@ class Player < ActiveRecord::Base
 
   scope :picked, where( "team_id is NOT ?", nil )
 
-  scope :for_pick, lambda {|pick_number| where("pick = ?", pick_number) } 
-  
-  scope :full_name, lambda {|name| where("first_name = ? AND last_name = ?", name.split[0], name.split[1])}
-  
-  scope :by_position, lambda {|position| where("position LIKE ?", "%#{position}") }
-  
-  def name
-    first_name + ' ' + last_name
+  scope :for_pick, lambda {|pick_number| where("pick = ?", pick_number) }
+
+  scope :full_name, lambda {|name| where("name = ?", name)}
+
+  scope :by_position, lambda {|position| where("position = ?", "#{position}") }
+
+  NFLTeamByeWeeks = {
+    'BAL' => 5, 'CLE' => 5, 'DAL' => 5, 'MIA' => 5, 'STL' => 5, 'WAS' => 5,
+    'ARI' => 6, 'DEN' => 6, 'KCC' => 6, 'SDC' => 6, 'SEA' => 6, 'TEN' => 6,
+    'BUF' => 7, 'CIN' => 7, 'NEP' => 7, 'NYG' => 7, 'PHI' => 7, 'SFO' => 7,
+    'ATL' => 8, 'CHI' => 8, 'GBP' => 8, 'NYJ' => 8, 'OAK' => 8, 'TBB' => 8,
+    'CAR' => 9, 'DET' => 9, 'JAC' => 9, 'MIN' => 9,
+    'HOU' => 11, 'IND' => 11, 'NOS' => 11, 'PIT' => 11
+  }
+
+  def self.update_rankings
+    draft_positions = Nokogiri::XML(open("http://football.myfantasyleague.com/#{Time.now.year}/export?TYPE=adp&IS_PPR=1&IS_MOCK=0"))
+
+    draft_positions.xpath("//player").each do |mfl_player|
+      player = Player.find_by_mfl_player_id(mfl_player.attributes['id'].value)
+      next if player.nil?
+
+      player.update_attributes(
+        :average_pick => mfl_player.attributes['averagePick'].value,
+        :minimum_pick => mfl_player.attributes['minPick'].value,
+        :maximum_pick => mfl_player.attributes['maxPick'].value
+      )
+    end
+  end
+
+  def self.populate
+    players = Nokogiri::XML(open("http://football.myfantasyleague.com/#{Time.now.year}/export?TYPE=players&L=&W="))
+
+    players.xpath("//player").each do |mfl_player|
+      player_attributes = {
+        :mfl_player_id  => mfl_player.attributes['id'].value,
+        :name           => mfl_player.attributes['name'].value,
+        :position       => mfl_player.attributes['position'].value.upcase,
+        :nfl_team       => mfl_player.attributes['team'].value,
+        :bye_week       => NFLTeamByeWeeks[mfl_player.attributes['team'].value]
+      }
+
+      next unless ['QB','TE','WR','RB','PK','DEF'].include?(player_attributes[:position])
+
+      player = Player.find_by_mfl_player_id(player_attributes[:mfl_player_id])
+
+      if player.nil?
+        player = Player.create(player_attributes)
+      else
+        player.update_attributes(player_attributes)
+      end
+    end
   end
 end
--- lib/tasks/load_adp_rankings.rake
+++ lib/tasks/load_adp_rankings.rake
@@ -1,94 +1,14 @@
 require 'rubygems'
-require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
 require 'pp'
 
+desc 'Grabs all the player information from the My Fantasy League Site.'
+task :update_players => :environment do
+  Player.populate
+end
+
 desc 'Grabs all the rank information from the My Fantasy League ADP Site.'
-task :load_adp_rankings => :environment  do |t, args|
-  @agent = Mechanize.new
-  @players = []
-
-  @bye_weeks = {
-    'BAL' => 5,
-    'CLE' => 5,
-    'DAL' => 5,
-    'MIA' => 5,
-    'STL' => 5,
-    'WAS' => 5,
-    'ARI' => 6,
-    'DEN' => 6,
-    'KCC' => 6,
-    'SDC' => 6,
-    'SEA' => 6,
-    'TEN' => 6,
-    'BUF' => 7,
-    'CIN' => 7,
-    'NEP' => 7,
-    'NYG' => 7,
-    'PHI' => 7,
-    'SFO' => 7,
-    'ATL' => 8,
-    'CHI' => 8,
-    'GBP' => 8,
-    'NYJ' => 8,
-    'OAK' => 8,
-    'TBB' => 8,
-    'CAR' => 9,
-    'DET' => 9,
-    'JAC' => 9,
-    'MIN' => 9,
-    'HOU' => 11,
-    'IND' => 11,
-    'NOS' => 11,
-    'PIT' => 11
-  }
-
-  ['QB','TE','RB','WR','PK','DEF'].each do |position|
-    page = @agent.get("http://football.myfantasyleague.com/#{Time.now.year}/adp?COUNT=500&POS=#{position}&CUTOFF=5&FRANCHISES=-1&IS_PPR=-1&IS_KEEPER=0&IS_MOCK=-1&TIME=")
-    p page.body
-    rows = page.search('table.report tr')
-
-    rows.each do |row|
-      next if row.search('th').length > 0
-      player = {}
-
-      cells = row.css('td')
-
-      cells.each_with_index do |value, index|
-        field = case index
-        when 0
-          :rank
-        when 1
-          :player
-        when 2
-          :average_pick
-        when 3
-          :minimum_pick
-        when 4
-          :maximum_pick
-        when 5
-          :drafts_selected_in
-        end
-
-        player[field] = value.text
-        if field == :player
-          player[:url]  = "http://football.myfantasyleague.com/#{Time.now.year}/" + value.children[0].attributes['href'].value
-        end
-      end
-
-      player[:player] =~ /(.+),\s(.+)\s(.{3})\s(.{2,3})/
-      player[:last_name] = $1
-      player[:first_name] = $2
-      player[:nfl_team] = $3
-      player[:position] = $4
-
-      player[:bye_week] = @bye_weeks[player[:nfl_team]]
-
-      @players << player
-    end
-  end
-
-  @players.each do |player|
-    attributes = player.reject{|k,v| [:player, :drafts_selected_in].include? k }
-    Player.create(attributes)
-  end
+task :update_rankings => :environment  do
+  Player.update_rankings
 end
