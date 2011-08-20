c1ef8310217d5c854fcb223b71e31af370803b53
Robert Jackson <robertj@promedicalinc.com>
2011-08-20 01:38:27 -0400

    Only allow index on TeamsController.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,4 @@
 class TeamsController < ApplicationController
-  before_filter :authenticate, :only => [ :edit, :delete, :new, :update ]
   # GET /teams
   # GET /teams.xml
   def index
@@ -8,82 +7,6 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @teams }
-    end
-  end
-
-  # GET /teams/1
-  # GET /teams/1.xml
-  def show
-    @team = Team.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @team }
-    end
-  end
-
-  # GET /teams/new
-  # GET /teams/new.xml
-  def new
-    @team = Team.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @team }
-    end
-  end
-
-  # GET /teams/1/edit
-  def edit
-    @team = Team.find(params[:id])
-  end
-
-  # POST /teams
-  # POST /teams.xml
-  def create
-    @team = Team.new(params[:team])
-
-    respond_to do |format|
-      if @team.save
-        format.html { redirect_to(@team, :notice => 'Team was successfully created.') }
-        format.xml  { render :xml => @team, :status => :created, :location => @team }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /teams/1
-  # PUT /teams/1.xml
-  def update
-    @team = Team.find(params[:id])
-
-    respond_to do |format|
-      if @team.update_attributes(params[:team])
-        format.html { redirect_to(@team, :notice => 'Team was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @team.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /teams/1
-  # DELETE /teams/1.xml
-  def destroy
-    @team = Team.find(params[:id])
-    @team.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(teams_url) }
-      format.xml  { head :ok }
-    end
-  end
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "ransom"
     end
   end
 end
