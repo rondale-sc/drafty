13f42863570815925c9f56890754f08d07579803
Jonathan Jackson <jonj@promedicalinc.com>
2011-08-21 11:25:39 -0400

    Only show first lett er of injury status.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def player_link(player)
-    link_to "#{player.name} #{player.nfl_team}(#{player.bye_week}) #{player.position} #{"- <span style=\"color:red\">#{player.injury_status}</span>" if player.injury_status}".html_safe,
+    link_to "#{player.name} #{player.nfl_team}(#{player.bye_week}) #{player.position} #{"- <span style=\"color:red\">#{player.injury_status[0,1]}</span>" if player.injury_status}".html_safe,
             player.url, {:id => 'search', :target => "player_lookup"}
   end
 end
