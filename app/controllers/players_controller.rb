12875943642c636b7c8c82ce22c0cd32b63db5a9
Robert Jackson <robertj@promedicalinc.com>
2011-08-20 23:19:36 -0400

    Only show healthy players on the best listings.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,12 +2,12 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.xml
   def index
-    @quarter_b =  Player.available.quaterbacks.by_ranking.limit(5)
-    @running_b =  Player.available.running_backs.by_ranking.limit(5)
-    @place_k   =  Player.available.place_kickers.by_ranking.limit(5)
-    @tight_e   =  Player.available.tight_ends.by_ranking.limit(5)
-    @defense   =  Player.available.defenses.by_ranking.limit(5)
-    @wide_r    =  Player.available.wide_receivers.by_ranking.limit(5)
+    @quarter_b =  Player.available.healthy.quaterbacks.by_ranking.limit(5)
+    @running_b =  Player.available.healthy.running_backs.by_ranking.limit(5)
+    @place_k   =  Player.available.healthy.place_kickers.by_ranking.limit(5)
+    @tight_e   =  Player.available.healthy.tight_ends.by_ranking.limit(5)
+    @defense   =  Player.available.healthy.defenses.by_ranking.limit(5)
+    @wide_r    =  Player.available.healthy.wide_receivers.by_ranking.limit(5)
 
     if params[:selected]
       @players = @players.where(:selected => (params[:selected] == "1"))
